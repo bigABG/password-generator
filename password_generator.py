f647b332dfacf6a9d8fc6075aeda1308db397d3f
bigABG <abdelaziz.benghalia@gmail.com>
2024-07-11 01:04:31 +0100

    Added comments

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,7 @@
+# Importing the necessary libraries
 from random import randint
 
+# Generating password based off the length given
 def generate_password(l):
     password = ""
     for i in range(l):
@@ -7,6 +9,7 @@ def generate_password(l):
         password = password + chr(x)
     return password
 
+# Running the script
 if __name__ == "__main__":
     length = int(input("please type the length of your password : "))
     print(generate_password(length))
